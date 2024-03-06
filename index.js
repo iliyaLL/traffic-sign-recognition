1caafa4b120c3e0baeba75841d7dbc568c8c033f
Iliya Lim <limilia248@gmail.com>
2024-03-06 22:26:27 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,151 @@
+CLASSES = {
+  0: "Cycle route ahead warning\nThe \"Cycle Route Ahead Warning\" sign is a regulatory traffic sign that alerts drivers to the presence of a designated cycle route ahead. This sign is used to notify drivers to be cautious and watch out for cyclists who may be using the road or designated lane for cycling purposes.\nAccording to paragraph 24 of the Rules of the Road, cyclists are prohibited from:\nRiding without holding onto the handlebars with at least one hand.\nCarrying passengers.\nTransporting cargo that protrudes more than 50 centimeters in length or width beyond the dimensions, or cargo that interferes with control.\nTurning left or making a U-turn on roads with more than one lane in the direction of travel.\nAt unregulated intersections of bicycle paths with roads located outside the intersection, cyclists must yield to vehicles traveling on this road.\nOperating a bicycle on highways.",
+  1: "End of zone with all restrictions\n\nRoad sign 3.31. End of zone with all restrictions. This sign cancels the effect of the following prohibitory signs (installed together or sequentially):\n\n3.16 Minimum distance restriction;\n3.20 No overtaking;\n3.22 No overtaking for trucks;\n3.24 Maximum speed limit;\n3.26 No horn blowing;\n3.27 No stopping;\n3.28 No parking;\n3.29 No parking on odd-numbered days of the month;\n3.30 No parking on even-numbered days of the month.",
+  2: "Give way\n\nThe \"Give Way\" sign belongs to the priority signs category. It indicates that at the intersection, unequal roads converge, requiring the driver to yield to other vehicles before proceeding.\n\nFailure to comply with the requirement of giving way to vehicles enjoying priority right of way, except in cases provided for by Part 2 of Article 594 and Article 598 of the Administrative Offenses Code of the Republic of Kazakhstan, entails a fine of 15 MRP (45,945 tenge).",
+  3: "Go straight ot turn left\n\nThe instruction \"Go straight or turn left\" indicates that at the upcoming intersection, drivers have the option to continue driving straight ahead or to make a left turn. This typically implies that there is no mandatory right turn at the intersection and that drivers have the choice to proceed straight or make a left turn.\n\nAs for potential infractions related to this instruction and corresponding fines according to the Traffic Laws in Kazakhstan:\n\n-Failure to follow the indicated direction: If a driver fails to follow the instruction to go straight or turn left and instead makes a right turn or takes another unauthorized action, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation.",
+  4: "Go straight or turn right\n\nThe instruction \"Go straight or turn right\" indicates that at the upcoming intersection, drivers have the option to continue driving straight ahead or to make a right turn. This typically implies that there is no mandatory left turn at the intersection and that drivers have the choice to proceed straight or make a right turn.\n\nAs for potential infractions related to this instruction and corresponding fines according to the Traffic Laws in Kazakhstan:\n\n-Failure to follow the indicated direction: If a driver fails to follow the instruction to go straight or turn right and instead makes a left turn or takes another unauthorized action, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation.",
+  5: "Stop sign\n\nThe \"Stop\" sign is a regulatory traffic sign that commands drivers to come to a complete halt at the marked stop line or before entering the intersection. This sign is used to control traffic flow and prevent accidents at intersections where there may be conflicting movements of vehicles or pedestrians.\n\nThere may be exceptions for certain situations, such as when directed by a traffic officer or when it is unsafe to come to a sudden stop, such as in icy road conditions.\n\nAs for potential infractions related to this sign and corresponding fines according to the Traffic Laws in Kazakhstan:\n\n-Failure to come to a complete stop: If a driver fails to come to a complete stop at the \"Stop\" sign, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 10000 to 20000 tenge (approximately $24 to $48 USD), depending on the severity of the violation.",
+  6: "Round-about\n\nEntering the roundabout: If there are no road signs 5.8.1 and 5.8.2 indicating lane directions, vehicles can enter the roundabout from any lane. This is specified in section 8.6 of the Traffic Rules: \"Before turning right, left, or making a U-turn, the driver takes the corresponding position on the roadway and in the lane intended for movement in that direction, except when turning when entering a roundabout.\" If signs 5.8.1 and 5.8.2 are present, drivers should follow the designated lanes as indicated on the signs.\n\nEntry from any lane, exit only to the right: Drivers can enter the roundabout from any lane, but they must exit to the right.\n\nYielding to other vehicles: Priority at the roundabout is determined by road signs such as \"Main Road\" and \"Give Way.\" If there are no priority signs, the rule of \"yield to the right\" applies. Drivers must give way to vehicles approaching from the right.\n\nUse of turn signals: When entering and exiting the roundabout, drivers should activate their right turn signal.\n\nRegarding fines for violations related to roundabout traffic rules in Kazakhstan:\n\n-Entering the roundabout from an incorrect lane: If a driver enters the roundabout from a lane that is not designated for their intended direction of travel or if they fail to follow the designated lanes indicated by signs 5.8.1 and 5.8.2, they may be fined. The fine for this offense typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD).",
+  7: "No entry\n\nThe \"No Entry\" sign is a regulatory traffic sign indicating that vehicles are prohibited from entering a particular road or area. It is typically used to prevent vehicles from entering a specific road, street, or area for various reasons, such as safety, traffic management, or to designate a one-way street.\n\nAs for potential infractions related to this sign and corresponding fines according to the Traffic Laws in Kazakhstan:\n\n-Ignoring the No Entry sign: If a driver disregards the \"No Entry\" sign and enters the restricted area, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation.\n\nIt's essential for drivers to observe and comply with traffic signs to ensure safety and legal compliance on the roads. Disregarding signs like \"No Entry\" can endanger oneself and others and may lead to legal consequences.",
+  8: "Straight ahead only\n\nThe \"Straight Ahead Only\" sign is a regulatory traffic sign that indicates to drivers that they must proceed only in the direction straight ahead, without making any turns. This sign is typically used to control traffic flow at intersections or roadways where turning movements are restricted for safety or traffic management reasons.\n\nThere may be exceptions or allowances for specific types of vehicles, such as emergency vehicles or public transportation, which would be indicated by supplementary signs.\n\nAs for potential infractions related to this sign and corresponding fines according to the Traffic Laws in Kazakhstan:\n\n-Failure to follow the indicated direction: If a driver disregards the \"Straight Ahead Only\" sign and makes a turn instead of proceeding straight ahead, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation.",
+  9: "Truck traffic prohibited\n\nThe \"Truck Traffic Prohibited\" sign is a regulatory traffic sign that indicates that trucks or other heavy vehicles are not allowed to enter a specific road or area. This sign is typically used to regulate traffic flow, prevent damage to roads, or ensure safety on certain roads where trucks may pose a hazard.\n\nThere may be exceptions for certain types of trucks, such as those making deliveries to properties within the restricted area or for emergency vehicles.\n\nViolation of truck traffic prohibition: If a truck driver ignores the \"Truck Traffic Prohibited\" sign and enters the restricted area, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 10000 to 20000 tenge (approximately $24 to $48 USD), depending on the severity of the violation.",
+  10: "Turn left ahead\n\nThe \"Turn Left Ahead\" sign is a regulatory traffic sign that indicates to drivers that a left turn is imminent ahead. This sign is used to provide advance warning to drivers, allowing them to prepare for the upcoming left turn by positioning their vehicle correctly and signaling their intent to turn left.\n\nIllegal left turn: If a driver makes an illegal left turn where prohibited by traffic signs or signals, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation",
+  11: "Turn right ahead\n\nThe \"Turn Right Ahead\" sign is a regulatory traffic sign that indicates to drivers that a right turn is imminent ahead. This sign is used to provide advance warning to drivers, allowing them to prepare for the upcoming right turn by positioning their vehicle correctly and signaling their intent to turn right.\n\nIllegal right turn: If a driver makes an illegal right turn where prohibited by traffic signs or signals, they may be fined according to the traffic regulations. The fine for this offense in Kazakhstan typically ranges from 5000 to 10000 tenge (approximately $12 to $24 USD), depending on the severity of the violation."
+};
+
+
+const MODEL_PATH =
+    'model/model.json';
+
+const IMAGE_SIZE = 416;
+const TOPK_PREDICTIONS = 1;
+
+let my_model;
+const demo = async () => {
+  status('Loading model...');
+
+  my_model = await tf.loadLayersModel(MODEL_PATH);
+
+  my_model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+
+  status('');
+
+  const catElement = document.getElementById('cat');
+  if (catElement.complete && catElement.naturalHeight !== 0) {
+    predict(catElement);
+    catElement.style.display = '';
+  } else {
+    catElement.onload = () => {
+      predict(catElement);
+      catElement.style.display = '';
+    }
+  }
+
+  document.getElementById('file-container').style.display = '';
+};
+
+async function predict(imgElement) {
+  status('Predicting...');
+
+  const startTime1 = performance.now();
+  let startTime2;
+  const logits = tf.tidy(() => {
+    const img = tf.browser.fromPixels(imgElement).toFloat();
+
+    const normalized = img.div(255.0);
+
+    const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+
+    startTime2 = performance.now();
+    return my_model.predict(batched);
+  });
+
+  const topClass = await getTopKClasses(logits, TOPK_PREDICTIONS);
+  const totalTime1 = performance.now() - startTime1;
+  const totalTime2 = performance.now() - startTime2;
+  status(`Done in ${Math.floor(totalTime1)} ms ` +
+      `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
+
+  showResults(imgElement, topClass);
+}
+async function getTopKClasses(logits, topK) {
+  const values = await logits.data();
+
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({value: values[i], index: i});
+  }
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  topkValues[0] = valuesAndIndices[0].value;
+  topkIndices[0] = valuesAndIndices[0].index;
+
+  const topClass = {
+    className: CLASSES[topkIndices[0]],
+    probability: topkValues[0]
+  };
+
+  console.log(topClass);
+  return topClass;
+}
+function showResults(imgElement, classes) {
+  const predictionContainer = document.createElement('div');
+  predictionContainer.className = 'pred-container';
+
+  const imgContainer = document.createElement('div');
+  imgContainer.appendChild(imgElement);
+  predictionContainer.appendChild(imgContainer);
+
+  const probsContainer = document.createElement('div');
+  const row = document.createElement('div');
+  row.className = 'row';
+
+  console.log(classes.probability);
+
+  const probsElement = document.createElement('div');
+  probsElement.className = 'cell';
+  probsElement.innerText = classes.probability.toFixed(3);
+  row.appendChild(probsElement);
+
+  const classElement = document.createElement('div');
+  classElement.className = 'cell';
+  classElement.innerText = classes.className;
+  row.appendChild(classElement);
+
+  probsContainer.appendChild(row);
+  predictionContainer.appendChild(probsContainer);
+
+  predictionsElement.insertBefore(
+      predictionContainer, predictionsElement.firstChild);
+}
+
+const filesElement = document.getElementById('files');
+filesElement.addEventListener('change', evt => {
+  let files = evt.target.files;
+  for (let i = 0, f; f = files[i]; i++) {
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+    let reader = new FileReader();
+    const idx = i;
+    reader.onload = e => {
+      let img = document.createElement('img');
+      img.src = e.target.result;
+      img.width = IMAGE_SIZE;
+      img.height = IMAGE_SIZE;
+      img.onload = () => predict(img);
+    };
+
+    reader.readAsDataURL(f);
+  }
+});
+
+const demoStatusElement = document.getElementById('status');
+const status = msg => demoStatusElement.innerText = msg;
+
+const predictionsElement = document.getElementById('predictions');
+
+demo();
